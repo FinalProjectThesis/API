0896c4256b59952e201a25afc641300fd1dff248
Joash Diaz <johnjoashcolangodiaz@gmail.com>
2022-01-18 17:38:04 +0800

    added first_name and last_name. also changed the return string to "SameUsername" so you can use that in apps

--- routes/register.js
+++ routes/register.js
@@ -14,12 +14,14 @@ router.post('/', async (req,res)=>{
     const post = new Post({
         username: req.body.username,
         password: req.body.password,
+        first_name:req.body.first_name,
+        last_name: req.body.last_name
     })
     const username1= req.body.username
     const Check = await Post.findOne({'username': username1})
     console.log(Check);
     if (Check){
-        res.json('There is already a user with the same Username!');
+        res.json('SameUsername'); //Someone Has the Same Username
     }else{
         post.save()
         .then(data => {
