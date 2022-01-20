761205cf420da8a1fd28bf0706b649ce06c98501
Joash Diaz <johnjoashcolangodiaz@gmail.com>
2022-01-20 16:08:44 +0800

    added different operations that can be queried

--- routes/ScoreList.js
+++ routes/ScoreList.js
@@ -5,8 +5,9 @@ var connection = require('../config.js');
 require('dotenv/config');
 
 router.post('/', async (req,res)=>{
+    var operation = req.body.operation;
     var student_id= req.body.student_id;
-    let sql = `SELECT * FROM Scores WHERE student_id='${student_id}'`
+    let sql = `SELECT * FROM Scores WHERE student_id='${student_id}' AND operation='${operation}'`
     connection.query(sql,function (err, result, fields) {
         if (err) {
             console.log(err);
